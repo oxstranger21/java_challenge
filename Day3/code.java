aca30db176ca9aabac6686cb8c8a5f9461f7030e
oxstranger21 <gopi2023@gift.edu.in>
2026-01-02 15:12:06 +0530

    day 3

---
+++ Day3/code.java
@@ -0,0 +1,12 @@
+//Check if even/odd and positive/negative
+package Day3;
+
+import java.util.Scanner;
+
+public class code {
+    public static void main(String[] args) {
+        System.out.println("Enter a number : ");
+        int num =  new Scanner(System.in).nextInt();
+
+    }
+}
