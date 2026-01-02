78722eedc73855804f175e7ac6ab82218a1eaf8f
oxstranger21 <gopi2023@gift.edu.in>
2026-01-02 15:30:03 +0530

    day 3

--- Day3/code.java
+++ Day3/code.java
@@ -6,7 +6,19 @@ import java.util.Scanner;
 public class code {
     public static void main(String[] args) {
         System.out.println("Enter a number : ");
-        int num =  new Scanner(System.in).nextInt();
+        int num = new Scanner(System.in).nextInt();
+        if (num % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+
+        if(num>0){
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative");
+        }
 
     }
 }
